f9c65f23240d05a68ed876d4bc04958a67764f36
Samuel Laheux <samuel.laheux@novadiscovery.com>
2021-04-22 19:03:12 +0200

    refactor: Board component contains only board behavior. Move history to Game component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,59 +1,15 @@
 import React from 'react';
 import '../index.css';
 import Square from './Square.js';
-import { calculateWinner } from '../utils'
 
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: this.resetSquares(),
-      xIsNext: true,
-      history: []
-    }
-  }
-
-  get nextPlayer() {
-    return `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-  }
-
-  get winner() {
-    const winner = calculateWinner(this.state.squares)
-    if (winner) return `The winner is: ${winner}`;
-    return null
-  }
-
-  resetSquares() {
-    return Array(9).fill(null)
-  }
-
-  handleUndoClick() {
-    if (this.state.history.length) {
-      const history = [ ...this.state.history ]
-      history.pop()
-      const squares = history.length > 0 ? history[history.length - 1] : this.resetSquares()
-      this.setState({ history, squares, xIsNext: !this.state.xIsNext })
-    }
-  }
-
-  handleClick(i) {
-    let squares = [ ...this.state.squares ]
-    let history = [ ...this.state.history ]
-
-    // If a player has already won or if the cell is already filled, do nothing 
-    if (!squares[i] && !this.winner) {
-      squares[i] = this.state.xIsNext ? 'X' : 'O'
-      history.push(squares)
-      this.setState({ squares, xIsNext: !this.state.xIsNext, history })
-    }
-  }
 
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.handleClick(i)}
       />
     );
   }
@@ -61,7 +17,6 @@ export default class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="status">{this.winner ?? this.nextPlayer}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -76,9 +31,6 @@ export default class Board extends React.Component {
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-        </div>
-        <div>
-          <button onClick={() => this.handleUndoClick()}>Undo</button>
         </div>
       </div>
     );
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import '../index.css';
 import Board from './Board.js';
+import { calculateWinner } from '../utils';
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: this.resetSquares(),
+      xIsNext: true,
+      history: []
+    }
+  }
+
+  get nextPlayer() {
+    return `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+  }
+
+  get winner() {
+    const winner = calculateWinner(this.state.squares)
+    if (winner) return `The winner is: ${winner}`;
+    return null
+  }
+
+
+  resetSquares() {
+    return Array(9).fill(null)
+  }
+
+  handleUndoClick() {
+    if (this.state.history.length) {
+      const history = [ ...this.state.history ]
+      history.pop()
+      const squares = history.length > 0 ? history[history.length - 1] : this.resetSquares()
+      this.setState({ history, squares, xIsNext: !this.state.xIsNext })
+    }
+  }
+
+
+  handleClick(i) {
+    let squares = [ ...this.state.squares ]
+    let history = [ ...this.state.history ]
+
+    // If a player has already won or if the cell is already filled, do nothing 
+    if (!squares[i] && !this.winner) {
+      squares[i] = this.state.xIsNext ? 'X' : 'O'
+      history.push(squares)
+      this.setState({ squares, xIsNext: !this.state.xIsNext, history })
+    }
+  }
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={this.state.squares}
+            handleClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div className="status">{this.winner ?? this.nextPlayer}</div>
           <ol>{/* TODO */}</ol>
+        </div>
+        <div>
+          <button onClick={() => this.handleUndoClick()}>Undo</button>
         </div>
       </div>
     );
